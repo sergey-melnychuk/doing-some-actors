ea1ce907dbf7456cd3b9fb84b39beefb206b5cc8
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2020-03-15 20:57:41 +0100

    Add Acc message type with message 10x fan-out

--- src/api.rs
+++ src/api.rs
@@ -23,16 +23,29 @@ impl Round {
 
 struct Hit(usize);
 
+#[derive(Clone)]
+struct Acc {
+    name: String,
+    hits: usize,
+}
+
 impl AnyActor for Round {
     fn receive(&mut self, envelope: Envelope, sender: &mut dyn AnySender) {
         if let Some(hit) = envelope.message.downcast_ref::<Hit>() {
             let mut rng = rand::thread_rng();
             let next = rng.gen_range(0, self.size);
             let tag = format!("{}", next);
-            println!("tag:'{}' hit:{} next:'{}'", self.tag, hit.0, tag);
-
+            //println!("tag:'{}' hit:{} next:'{}'", self.tag, hit.0, tag);
             let envelope = Envelope { message: Box::new(Hit(hit.0 + 1)), from: self.tag.clone() };
             sender.send(&tag, envelope);
+        } else if let Some(acc) = envelope.message.downcast_ref::<Acc>() {
+            //println!("tag:'{}' acc: name='{}' hits={}", self.tag, acc.name, acc.hits);
+            let nxt = Acc{ name: acc.name.clone(), hits: acc.hits + 1 };
+            for id in 0..self.size {
+                let tag = format!("{}", id);
+                let env = Envelope { message: Box::new(nxt.clone()), from: self.tag.clone() };
+                sender.send(&tag, env)
+            }
         } else {
             println!("unexpected message: {:?}", envelope.message.type_id());
         }
@@ -56,8 +69,13 @@ pub fn run() {
         let tag = format!("{}", id);
         scheduler.spawn(&tag, |tag| Box::new(Round::new(tag, SIZE)));
     }
-
     scheduler.send("0", Envelope { message: Box::new(Hit(0)), from: String::default() });
+
+    for id in 0..SIZE {
+        let tag = format!("{}", id);
+        let env = Envelope { message: Box::new(Acc { name: tag.clone(), hits: 0 }), from: tag.clone() };
+        scheduler.send(&tag, env);
+    }
 
     let mut pool = ThreadPool::new(num_cpus::get());
     start_actor_runtime(pool, scheduler);
